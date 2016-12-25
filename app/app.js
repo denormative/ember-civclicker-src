28f1c401f4b142f5079cb0ccff13fef8180d11a0
Patrick Burke <denormative@gmail.com>
2016-12-25 20:18:21 +0900

    trying to move some of this stuff into ember...

--- app/app.js
+++ app/app.js
@@ -13,7 +13,8 @@ App = Ember.Application.extend({
   podModulePrefix: config.podModulePrefix,
   ready: function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-      initCivclicker();
+      this.initCivClicker();
+      console.log("running");
       $('.menu .item')
         .tab()
       ;
@@ -23,10 +24,93 @@ App = Ember.Application.extend({
   },
   tick: function(){
     var self = this;
-    runLoop();
+    this.runLoop();
     Ember.run.later(function(){
       self.tick();
     }, 1000);
+  },
+  initCivClicker: function() {
+    // Start of init program code
+    document.title = "CivClicker ("+versionData+")"; //xxx Not in XML DOM.
+
+    addUITable(basicResources, "basicResources"); // Dynamically create the basic resource table.
+    addUITable(homeBuildings, "buildings"); // Dynamically create the building controls table.
+    addUITable(homeUnits, "jobs"); // Dynamically create the job controls table.
+    addUITable(armyUnits, "party"); // Dynamically create the party controls table.
+    addUpgradeRows(); // This sets up the framework for the upgrade items.
+    addUITable(normalUpgrades, "upgrades"); // Place the stubs for most upgrades under the upgrades tab.
+    addAchievementRows();
+    addRaidRows();
+    addWonderSelectText();
+    makeDeitiesTables();
+
+    if (!load("localStorage")) { //immediately attempts to load
+        //Prompt player for names
+        renameCiv();
+        renameRuler();
+    }
+    updateSettings();
+  },
+  runLoop: function() {
+    // This sets up the main game loop, which is scheduled to execute once per second.
+    //debugging - mark beginning of loop execution
+    //var start = new Date().getTime();
+
+    tickAutosave();
+
+    // Production workers do their thing.
+    doFarmers();
+    doWoodcutters();
+    doMiners();
+    doBlacksmiths();
+    doTanners();
+    doClerics();
+
+    // Check for starvation
+    doStarve();
+    //xxx Need to kill workers who die from exposure.
+
+    //Resources occasionally go above their caps.
+    //Cull the excess /after/ other workers have taken their inputs.
+    resourceData.forEach( function(elem){ if (elem.owned > elem.limit) { elem.owned = elem.limit; } });
+
+    //Timers - routines that do not occur every second
+    doMobs();
+    doPestControl();
+    tickGlory();
+    doShades();
+    doEsiege(civData.esiege, civData.fortification);
+    doRaid("party","player","enemy");
+
+    //Population-related
+    doGraveyards();
+    doHealers();
+    doCorpses();
+    doThrone();
+    tickGrace();
+    tickWalk();
+    doLabourers();
+    tickTraders();
+
+    updateResourceTotals(); //This is the point where the page is updated with new resource totals
+    testAchievements();
+
+    //Data changes should be done; now update the UI.
+    updateUpgrades();
+    updateResourceRows(); //Update resource display
+    updateBuildingButtons();
+    updateJobButtons();
+    updatePartyButtons();
+    updatePopulationUI();
+    updateTargets();
+    updateDevotion();
+    updateWonder();
+    updateReset();
+
+    //Debugging - mark end of main loop and calculate delta in milliseconds
+    //var end = new Date().getTime();
+    //var time = end - start;
+    //console.log("Main loop execution time: " + time + "ms");
   },
   Resolver
 });
