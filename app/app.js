e437273e6cfd3d3901538367f656297b9dc27c70
Patrick Burke <denormative@gmail.com>
2017-01-04 20:25:14 +0900

    jslint -> eslint

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+const { $ } = Ember;
 
 let App;
 
--- app/helpers/prettynum.js
+++ app/helpers/prettynum.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+/*global settings*/
 
-export function prettynum([value, ...rest]) {
+export function prettynum([value]) {
   //xxx TODO: Add appropriate format options
-  return (settings.delimiters) ? Number(value).toLocaleString() : input.toString();
+  return (settings.delimiters) ? Number(value).toLocaleString() : value.toString();
 }
 
 export default Ember.Helper.helper(prettynum);
